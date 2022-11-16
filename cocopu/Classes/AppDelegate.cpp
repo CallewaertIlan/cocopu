e8aced0100c99a0e88e34aa96060d313183cae55
CallewaertIlan <ilan.callewaert.pro@gmail.com>
2022-11-16 13:25:47 +0100

    add window size

--- cocopu/Classes/AppDelegate.cpp
+++ cocopu/Classes/AppDelegate.cpp
@@ -1,3 +1,4 @@
+
 #include "AppDelegate.h"
 #include "HelloWorldScene.h"
 
@@ -49,7 +50,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto glview = director->getOpenGLView();
     if(!glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glview = GLViewImpl::createWithRect("cocopu", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+        glview = GLViewImpl::createWithRect("cocopu", cocos2d::Rect(0, 0, mediumResolutionSize.width, mediumResolutionSize.height));
 #else
         glview = GLViewImpl::create("cocopu");
 #endif
