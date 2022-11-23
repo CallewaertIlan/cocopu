11ce42417e2acf26e5e1baa8b572c5f33edd0ab4
unknown <ecoumel@gamingcampus.local>
2022-11-23 16:02:26 +0100

    add HUD

--- cocopu/proj.win32/HUD.cpp
+++ cocopu/proj.win32/HUD.cpp
@@ -1,1 +1,134 @@
-#include "HUD.h"
+#include "includes.h"
+
+HUD::HUD()
+{
+}
+
+HUD::~HUD()
+{
+}
+
+void HUD::update(float)
+{
+}
+
+void HUD::initialisation() 
+{
+    createJumpButton(300, 37);
+    createGlideButton(400, 37);
+    createBlockButton(500, 37);
+    createDigButton(600, 37);
+    createExplosionButton(700, 37);
+     
+}
+
+void HUD::createJumpButton(float x, float y)
+{
+    auto button = ui::Button::create("JumpButton.png", "JumpButtonSelected.png");
+    button->setPosition(Vec2(x, y));
+    button->setScale(0.35);
+    
+
+    button->addTouchEventListener([&](Ref* sender, ui::Widget::TouchEventType type) {
+        switch (type)
+        {
+        case ui::Widget::TouchEventType::BEGAN:
+            break;
+        case ui::Widget::TouchEventType::ENDED:
+            std::cout << "Jump button clicked" << std::endl;
+            break;
+        default:
+            break;
+        }
+        });
+
+    addChild(button);
+}
+void HUD::createGlideButton(float x, float y)
+{
+    auto button = ui::Button::create("GlideButton.png", "GlideButtonSelected.png");
+    button->setPosition(Vec2(x, y));
+    button->setScale(0.35);
+
+
+    button->addTouchEventListener([&](Ref* sender, ui::Widget::TouchEventType type) {
+        switch (type)
+        {
+        case ui::Widget::TouchEventType::BEGAN:
+            break;
+        case ui::Widget::TouchEventType::ENDED:
+            std::cout << "Glide button clicked" << std::endl;
+            break;
+        default:
+            break;
+        }
+        });
+
+    addChild(button);
+}
+void HUD::createBlockButton(float x, float y)
+{
+    auto button = ui::Button::create("BlockButton.png", "BlockButtonSelected.png");
+    button->setPosition(Vec2(x, y));
+    button->setScale(0.35);
+
+
+    button->addTouchEventListener([&](Ref* sender, ui::Widget::TouchEventType type) {
+        switch (type)
+        {
+        case ui::Widget::TouchEventType::BEGAN:
+            break;
+        case ui::Widget::TouchEventType::ENDED:
+            std::cout << "Block button clicked" << std::endl;
+            break;
+        default:
+            break;
+        }
+        });
+
+    addChild(button);
+}
+void HUD::createDigButton(float x, float y)
+{
+    auto button = ui::Button::create("DigButton.png", "DigButtonSelected.png");
+    button->setPosition(Vec2(x, y));
+    button->setScale(0.35);
+
+
+    button->addTouchEventListener([&](Ref* sender, ui::Widget::TouchEventType type) {
+        switch (type)
+        {
+        case ui::Widget::TouchEventType::BEGAN:
+            break;
+        case ui::Widget::TouchEventType::ENDED:
+            std::cout << "Dig button clicked" << std::endl;
+            break;
+        default:
+            break;
+        }
+        });
+
+    addChild(button);
+}
+void HUD::createExplosionButton(float x, float y)
+{
+    auto button = ui::Button::create("ExplosionButton.png", "ExplosionButtonSelected.png");
+    button->setPosition(Vec2(x, y));
+    button->setScale(0.35);
+
+
+    button->addTouchEventListener([&](Ref* sender, ui::Widget::TouchEventType type) {
+        switch (type)
+        {
+        case ui::Widget::TouchEventType::BEGAN:
+            break;
+        case ui::Widget::TouchEventType::ENDED:
+            std::cout << "Explosion button clicked" << std::endl;
+            break;
+        default:
+            break;
+        }
+        });
+
+    addChild(button);
+}
--- cocopu/proj.win32/HUD.h
+++ cocopu/proj.win32/HUD.h
@@ -1,5 +1,17 @@
 #pragma once
-class HUD
+class HUD : public Layer
 {
+public:
+	HUD();
+	~HUD();
+	void initialisation();
+	void update(float) override;
+	void createJumpButton(float x, float y);
+	void createBlockButton(float x, float y);
+	void createExplosionButton(float x, float y);
+	void createDigButton(float x, float y);
+	void createGlideButton(float x, float y);
+	CREATE_FUNC(HUD);
+private:
+	float m_time;
 };
-
